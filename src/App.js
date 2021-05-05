41882d1f3c0cc25c813b758cd3349b3ded5f714f
hhhhhq <Mr.j@tom.com>
2021-05-05 23:43:19 +0800

    react-icon finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { FaReact } from "react-icons/fa"
+import { MdAttachMoney } from "react-icons/md"
+import { IconContext } from "react-icons"
+
+const color = ["green"]
+const size = ["6rem"]
 function App() {
-  return <div className="App">111</div>
+  return (
+    <div className="App">
+      <IconContext.Provider value={{ color: color[0], size: size[0] }}>
+        <FaReact />
+      </IconContext.Provider>
+      <div>
+        <IconContext.Provider value={{ color: color[0], size: size[0] }}>
+          <MdAttachMoney />
+        </IconContext.Provider>
+      </div>
+    </div>
+  )
 }
 
 export default App

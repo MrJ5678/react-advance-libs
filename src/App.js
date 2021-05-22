4c8a6d9b49a40ae205bb7527636c9ce205f6797e
hhhhhq <Mr.j@tom.com>
2021-05-22 09:36:52 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import CountUp, { useCountUp } from "react-countup"
+
 function App() {
-  return <div className="App">111</div>
+  const { countUp, start, pauseResume, reset, update } = useCountUp({
+    duration: 5,
+    end: 10000,
+  })
+  return (
+    <div className="App">
+      <div>
+        <div>{countUp}</div>
+        <button onClick={start}>Start</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={pauseResume}>Pause/Resume</button>
+        <button onClick={() => update(2000)}>Update to 2000</button>
+      </div>
+      <CountUp end={100} />
+      <br />
+      <CountUp end={200} duration={5} />
+    </div>
+  )
 }
 
 export default App

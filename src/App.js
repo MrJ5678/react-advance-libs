7584fd15b8b533db9d914c8fbe416f8124b1f73b
hhhhhq <Mr.j@tom.com>
2021-05-22 10:31:44 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { ClipLoader, ClockLoader, GridLoader } from "react-spinners"
+import { css } from "@emotion/react"
+
+const loaderCSS = css`
+  margin-top: 25px;
+  margin-bottom: 25px;
+`
+
 function App() {
-  return <div className="App">111</div>
+  return (
+    <div className="App">
+      <p>qiuzhi99</p>
+      <ClipLoader css={loaderCSS} size={24} />
+      <br />
+      <ClockLoader css={loaderCSS} loading size={56} color="purple" />
+      <br />
+      <GridLoader css={loaderCSS} size={78} />
+    </div>
+  )
 }
 
 export default App

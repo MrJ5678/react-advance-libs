abed00d8f55c15d8884c888911bc577cd4e9975e
hhhhhq <Mr.j@tom.com>
2021-05-22 09:56:22 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState } from "react"
+import { ChromePicker, SketchPicker, PhotoshopPicker } from "react-color"
+
 function App() {
-  return <div className="App">111</div>
+  const [color, setColor] = useState("#fff")
+  const [showColorPicker, setShowColorPicker] = useState(false)
+  return (
+    <div className="App">
+      <button
+        onClick={() => setShowColorPicker(showColorPicker => !showColorPicker)}
+      >
+        {showColorPicker ? "Close" : "Pick"}
+      </button>
+      {showColorPicker && (
+        <ChromePicker
+          color={color}
+          onChange={updateColor => setColor(updateColor)}
+        />
+      )}
+      <h2>you pick {color.hex}</h2>
+    </div>
+  )
 }
 
 export default App

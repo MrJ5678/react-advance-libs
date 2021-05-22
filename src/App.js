34d9cd04074fcd4cc3e4ec0c936bbadf09cc02d9
hhhhhq <Mr.j@tom.com>
2021-05-22 10:19:25 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import ReactPlayer from "react-player"
+
 function App() {
-  return <div className="App">111</div>
+  return (
+    <div className="App">
+      <ReactPlayer
+        width="480px"
+        height="240px"
+        url="https://rails365-video.oss-cn-shenzhen.aliyuncs.com/episodes/prareact/reactp01..---1080p..mp4"
+        controls
+      />
+    </div>
+  )
 }
 
 export default App

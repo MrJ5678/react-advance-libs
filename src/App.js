71fe05406a6014e1aabe7a1de6c4090dc20be0a8
hhhhhq <Mr.j@tom.com>
2021-05-22 10:09:56 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react"
+import DatePicker, { registerLocale } from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import zhCN from "date-fns/locale/zh-CN"
+registerLocale("zh-CN", zhCN)
+
 function App() {
-  return <div className="App">111</div>
+  const [startDate, setStartDate] = useState(new Date())
+  return (
+    <div className="App">
+      <DatePicker
+        locale="zh-CN"
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+        dateFormat="dd/MM/yyyy"
+        isClearable
+        filterDate={date => date.getDay() !== 6 && date.getDay() !== 1}
+      />
+    </div>
+  )
 }
 
 export default App

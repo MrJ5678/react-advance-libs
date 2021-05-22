c334abdb69f391c6cf0c43f115c86b3f08336fa6
hhhhhq <Mr.j@tom.com>
2021-05-22 09:24:21 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Tippy from "@tippyjs/react"
+import "tippy.js/dist/tippy.css"
+
+const ColoredTooltip = () => {
+  return <span style={{ color: "orange" }}>Colored</span>
+}
+
 function App() {
-  return <div className="App">111</div>
+  return (
+    <div className="App">
+      <p>qiuzhi99</p>
+      <Tippy content="Hello">
+        <button>My button</button>
+      </Tippy>
+      <br />
+      <Tippy content={<ColoredTooltip />}>
+        <button>My button</button>
+      </Tippy>
+    </div>
+  )
 }
 
 export default App
